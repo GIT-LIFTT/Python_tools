e54cc541a20b3d4e843611a8737e6f6829d6e9c6
git-liftt <jonathan_s7@hotmail.co.uk>
2020-04-18 23:27:22 +0100

    git update

--- dictionary_with_nested-List_to_csv.py
+++ dictionary_with_nested-List_to_csv.py
@@ -18,11 +18,20 @@ with open('dict-to-csv.csv', mode='w', newline='') as csv_file:
     #dictionary passed to the writerow() method are written to file f
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-
+    #writes the headers
+    #Write a row with the field names (as specified in the constructor).
     writer.writeheader()
+
+    #as we defined earlier a list  of the keys from the opriginal dictionary
+    # this allows us to iterate through each key : value pair
+    # for k in the range of keys see above = 2
     for k in range(len(keys)):
      print(keys[k])
+
+     #  a for loop within a for loop  allows us to iterate through the elements within the current key
      for i in (dict[keys[k]]):
+
+      # if string value which = status is found assing to the varible i
       if ("status" in i):
          status = i
          print( "this is in the first list", status)
@@ -30,5 +39,7 @@ with open('dict-to-csv.csv', mode='w', newline='') as csv_file:
           availability = i
           print("this is in the second list", availability)
      print("writing row")
+
+     #write a row with the current values of the varibles within the given for loop
      writer.writerow(
         {'ID': keys[k], 'status': status, 'availability': availability})
